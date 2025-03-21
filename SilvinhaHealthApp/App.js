d54299afa88ed4914c73b66f95dcf90c4dd98207
varraschin <marcelojuniber@gmail.com>
2025-03-21 12:22:11 -0300

    Terminado

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -5,6 +5,27 @@ import { StyleSheet, Text, View, SafeAreaView, TextInput,} from 'react-native';
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
+  const [height, setHeight] = useState(null);
+  const [weight, setWeight] = useState(null);
+  const [imc, setImc] = useState(null);
+  const [messageImc, setMessageImc] = useState("Preencha o peso e a altura")
+  const [textButton, setTextButton] = useState("Calcular");
+
+  function calcularImc() {
+    if (weight != null && height != null){
+      Keyboard.dismiss();
+      setImc((weight / (height * height)).toFixed(2));
+      setHeight(null);
+      setWeight(null);
+      setMessageImc("Seu IMC é igual a");
+      setTextButton("Calcular novamente");
+    } else {
+      setImc(null);
+      setMessageImc("Preencha o peso e a altura");
+      setTextButton("Calcular");
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.titleContainer}>
@@ -17,6 +38,8 @@ export default function App() {
           <Text style={styles.label}>Altura</Text>
           <TextInput
             style={styles.input}
+            value={height ?? ''}
+            onChangeText={setHeight}
             placeholder='Ex. 1.70'
             keyboardType='numeric'
           ></TextInput>
@@ -25,6 +48,8 @@ export default function App() {
           <Text style={styles.label}>Peso</Text>
           <TextInput
             style={styles.input}
+            value={weight ?? ''}
+            onChangeText={setWeight}
             placeholder='Ex. 80.650'
             keyboardType='numeric'
           ></TextInput>
@@ -32,15 +57,15 @@ export default function App() {
 
         <TouchableOpacity 
           style={styles.button}
-          onPress={() => alert("ABACATE")}
+          onPress={() => calcularImc()}
         >
           <Ionicons name="calculator-sharp" size={24} color="#edf2f4" />
-          <Text style={styles.text}>Calcular</Text>
+          <Text style={styles.text}>{textButton}</Text>
         </TouchableOpacity>
 
         <View style={styles.imcContainer}>
-          <Text style={styles.imcText}>Preencha o peso e a altura</Text>
-          <Text style={styles.imcResult}>20</Text>
+          <Text style={styles.imcText}>{messageImc}</Text>
+          <Text style={styles.imcResult}>{imc}</Text>
         </View>
 
       </View>
